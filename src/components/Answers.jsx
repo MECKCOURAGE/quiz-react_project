9bf2024486a532d2a2d9c00f8706c084ce19e9db
courage meck <theme@DESKTOP-T1QIHLE.Prompt>
2024-10-03 15:36:40 +0100

    initial commit

---
+++ src/components/Answers.jsx
@@ -0,0 +1,30 @@
+import { useRef } from "react";
+export default function Answers ({answers, selectedAnswer, answerState, onSelect}){
+    const shuffedAnswers = useRef();
+    if (!shuffedAnswers.current) {
+        shuffedAnswers.current = [...answers];
+        shuffedAnswers.current.sort(() => Math.random() - 0.5);
+    }
+    return (
+        <ul id="answers">
+                {shuffedAnswers.current.map((answer) => {
+                    const isSelected = selectedAnswer === answer;
+                    let cssClass = '';
+
+
+                    if (answerState === 'answered' && isSelected) {
+                        cssClass = 'selected'
+                    }
+
+                    if ((answerState === 'correct' || answerState === 'wrong') && isSelected) {
+                        cssClass = answerState;
+                    }
+
+
+                    return <li key={answer} className="answer">
+                        <button onClick={() => onSelect(answer)} className={cssClass} disabled={answerState !== ''}>{answer}</button>
+                    </li>
+                })}
+            </ul>
+    )
+}
